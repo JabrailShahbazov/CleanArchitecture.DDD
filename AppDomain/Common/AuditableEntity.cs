d1d38098ff8d69fd219f8f958afc8ae994174b06
Cəbrayıl Şahbazov <cebrail.shahbazov@wcu.edu.az>
2021-05-16 17:13:55 +0400

    Added AuditEntity >Common

--- AppDomain/Common/AuditableEntity.cs
+++ AppDomain/Common/AuditableEntity.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace AppDomain.Common
 {
-    public class AuditableEntity
+    public abstract class AuditableEntity
     {
-        
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        public string LastModifiedBy { get; set; }
     }
 }
